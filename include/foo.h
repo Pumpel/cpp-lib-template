72e84a76164636dd86fb0c52111a0e65d2481c89
Pumpel <timopumpel@gmail.com>
2021-01-10 18:55:11 +0100

    Use old googletest download during configuration. Use GenerateExportHeader.

--- include/foo.h
+++ include/foo.h
@@ -1,16 +1,8 @@
 #pragma once
 
-//#if BUILDING_LIBFOO && HAVE_VISIBILITY
-//#define LIBFOO_DLL_EXPORTED __attribute__((__visibility__("default")))
-//#elif BUILDING_LIBFOO && defined _MSC_VER
-//#define LIBFOO_DLL_EXPORTED __declspec(dllexport)
-//#elif defined _MSC_VER
-//#define LIBFOO_DLL_EXPORTED __declspec(dllimport)
-//#else
-//#define LIBFOO_DLL_EXPORTED
-//#endif
+#include "foolib_export.h"
 
 namespace FooLib {
-  int __attribute__((__visibility__("default"))) foo(bool branch = false);
+  int FOOLIB_EXPORT foo(bool branch = false);
 }
 
