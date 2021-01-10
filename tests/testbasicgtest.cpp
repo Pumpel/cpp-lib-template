6b3db517049cdae61311bbf6172c03bbb3c5c477
twies <timo.wiessemann@gmail.com>
2021-01-10 02:00:33 +0100

    Stripping unwanted features of template 8/N
    Testing using googletest

---
+++ tests/testbasicgtest.cpp
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+
+TEST(basicGTest, testEqual) {
+  EXPECT_EQ(1, 1);
+}
+TEST(basicGTest, testTrue) {
+  EXPECT_TRUE(true);
+}
+
+TEST(basicGTest, testFalse) {
+  EXPECT_FALSE(false);
+}
